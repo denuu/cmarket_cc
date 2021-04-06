6925b911fe318f6b2304b926e9e7c724eb1ac7bd
Denuu <denis.nossevitch@gmail.com>
2021-04-05 21:53:37 -0700

    remove new web function

--- cmarket-cc/routes/web.php
+++ cmarket-cc/routes/web.php
@@ -14,8 +14,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/{any?}', App\Http\Controllers\PagesController::class);
-
-// TODO.
-Route::get('{any}', function () {
-    return view('app');
-})->where('any', '.*');
